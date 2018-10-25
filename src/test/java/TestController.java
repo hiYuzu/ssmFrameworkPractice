e11efb9dc4bd4e580b247c4c0fdca13edb32d1a2
hiYuzu <pomelo.yuzu1997@gmail.com>
2018-10-25 14:39:14 +0800

    测试controller功能 测试结果:正常

---
+++ src/test/java/TestController.java
@@ -0,0 +1,49 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * <p>
+ *
+ * @author hiYuzu
+ * @description </p>
+ * @date 2018/10/25 13:39
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-mybatis.xml", "classpath:spring-mvc.xml"})
+@WebAppConfiguration
+public class TestController {
+
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected WebApplicationContext webApplicationContext;
+
+    @Before()
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();  //初始化MockMvc对象
+    }
+
+    @Test
+    public void getUsersTest() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/UserController/queryUsers");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletRequest request = result.getRequest();
+        System.out.println("成功");
+        System.out.println("-----------------------------------");
+        System.out.println(request);
+        System.out.println("-----------------------------------");
+    }
+}
