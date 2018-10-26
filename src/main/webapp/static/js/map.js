0bd4821ad2d9c3f1f0f36759e9286ee09513ccbb
hiYuzu <pomelo.yuzu1997@gmail.com>
2018-10-26 16:07:39 +0800

    地图展示JS

--- src/main/webapp/static/js/map.js
+++ src/main/webapp/static/js/map.js
@@ -1,24 +1,16 @@
+showMap();
 function showMap() {
     var map = new BMap.Map("allmap");
-    map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
+    var point = new BMap.Point(117.1786889372559, 39.10762965106183);  // 初始化地图,设置中心点坐标和地图级别
+    map.centerAndZoom(point, 12)
     //添加地图类型控件
     map.addControl(new BMap.MapTypeControl({
         mapTypes:[
             BMAP_NORMAL_MAP,
             BMAP_HYBRID_MAP
         ]}));
-    map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
     map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
-}
-
-function showUser() {
-    var url1 = "/UserController/queryUsers";
-    $.ajax({
-        url: url1,
-        type: "get",
-        dataType: "json",
-        success: function (json) {
-            alert(JSON.stringify(json));
-        }
-    });
+    map.enableInertialDragging();
+    map.enableContinuousZoom();
+    map.setDefaultCursor("Default");
 }
