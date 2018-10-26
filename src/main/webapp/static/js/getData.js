8fc0216e3c102aef789e3beb0af565dc18b7879d
hiYuzu <pomelo.yuzu1997@gmail.com>
2018-10-26 16:08:34 +0800

    测试数据库信息交互js,关联main.html

---
+++ src/main/webapp/static/js/getData.js
@@ -0,0 +1,19 @@
+/**
+ *<p>
+ *@description json获取后台信息测试
+ *</p>
+ *@author hiYuzu
+ *@date 2018/10/26 15:58
+ */
+
+function getData() {
+    $.ajax({
+        url: "/UserController/queryUsers",
+        type: "get",
+        dataType: "json",
+        success: function (json) {
+            console.log(JSON.stringify(json));
+            alert("获取信息成功!");
+        }
+    })
+}
