78e44c42cdf3f6d979a84f4a2652df3aeae3035f
hiYuzu <pomelo.yuzu1997@gmail.com>
2018-11-07 10:43:52 +0800

    修改信息显示

--- src/main/webapp/static/js/loginSuccess.js
+++ src/main/webapp/static/js/loginSuccess.js
@@ -32,13 +32,13 @@ function getMessage() {
         type: "get",
         dataType: "json",
         success: function (json) {
-            var length = json.rows.length;
-            console.log("共有" + json.total + "条留言");
+            var length = json.total;
+            console.log("共有" + length + "条留言");
             for (var i = 0; i < length; i++) {
-                console.log("第" + json.rows[i].senderId + "条留言");
-                console.log("留言人:" + json.rows[i].senderName + "\n电话:" + json.rows[i].senderTel);
-                console.log("留言信息:" + json.rows[i].senderMsg);
-                console.log("留言时间" + json.rows[i].optTime);
+                console.log("第" + i+1 + "条留言");
+                console.log("留言人:\n" + json.rows[i].senderName + "\n电话:\n" + json.rows[i].senderTel);
+                console.log("留言信息:\n" + json.rows[i].senderMsg);
+                console.log("留言时间\n" + json.rows[i].optTime);
                 console.log("---------------------------------");
             }
         }
