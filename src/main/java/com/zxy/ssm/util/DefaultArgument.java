32a3c904ac164f0c1151e3019f3a2baafa7b5c6b
hiYuzu <pomelo.yuzu1997@gmail.com>
2018-12-19 09:24:38 +0800

    log-version-18.12.19.09

--- src/main/java/com/zxy/ssm/util/DefaultArgument.java
+++ src/main/java/com/zxy/ssm/util/DefaultArgument.java
@@ -18,4 +18,16 @@ public class DefaultArgument {
      * int默认值:-1
      */
     public static final int INT_DEFAULT = -1;
+    /**
+     * 和风天气(HeWeather)请求秘钥
+     * @owner hiYuzu
+     * @email pomelo.yuzu1997@gmail.com
+     * @authentication 个人开发者(普通用户-个人开发者-企业认证)
+     */
+    public static final String HE_KEY = "f84f1c36df6f45aa9977039826da641f";
+    /**
+     * 百度地图请求秘钥
+     * @owner 天津七一二通信广播股份有限公司
+     */
+    public static final String MAP_KEY = "AGl6mLfskQ2WZAD5dvD0Kls3mqsGBGcG";
 }
