a7ea1674e6cd9643f5dea51c80cde3153e5c2e1f
hiYuzu <pomelo.yuzu1997@gmail.com>
2019-03-11 11:05:39 +0800

    v19.3.11.11

--- src/main/java/com/zxy/ssm/util/DefaultArgument.java
+++ src/main/java/com/zxy/ssm/util/DefaultArgument.java
@@ -23,15 +23,26 @@ public class DefaultArgument {
      */
     public static final Double DOUBLE_DEFAULT = null;
     /**
+     * num默认值：7
+     */
+    public static final int NUM = 7;
+    /**
      * 和风天气(HeWeather)请求秘钥
      * @owner hiYuzu
      * @email pomelo.yuzu1997@gmail.com
      * @authentication 个人开发者(普通用户-个人开发者-企业认证)
      */
-    public static final String HE_KEY = "f84f1c36df6f45aa9977039826da641f";
+    public static final String HE_KEY = "aqn7aqMqT\u0081araqatUK]o`[r<U\u0081j~T\u0081r;Tqar`[`7T^`D";
     /**
      * 百度地图请求秘钥
      * @owner 天津七一二通信广播股份有限公司
      */
-    public static final String MAP_KEY = "4IU3oIAMpZhfWZsMu7xzqBBAf6vMHcoa";
+    public static final String MAP_KEY_1 = "ULs]T9@QX\\8~^tot]8w\u0081[_\\:lOw\u007FXrQIaqa9[\\oqi9LD";
+
+    /**
+     * 百度地图请求秘钥
+     * @owner hiYuzu
+     * @email pomelo.yuzu1997@gmail.com
+     */
+    public static final String MAP_KEY_2 = "XuULYOYp]tYThKQpj\\oOhskyU^X;]OYS[Lj7YNQSk:PD";
 }
--- src/main/java/com/zxy/ssm/util/EncodeUtil.java
+++ src/main/java/com/zxy/ssm/util/EncodeUtil.java
@@ -1,5 +1,8 @@
 package com.zxy.ssm.util;
 
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
 import java.security.MessageDigest;
 
 /**
@@ -28,5 +31,129 @@ public class EncodeUtil {
             stringBuffer.append(Integer.toHexString(0xff & resultCode[i]));
         }
         return stringBuffer.toString();
+    }
+
+    /**
+     * base64编码
+     * @param code
+     * @return
+     */
+    public static String base64Encode(String code) {
+        String result = null;
+        try {
+            byte[] byteArrayCode = code.getBytes("utf-8");
+            BASE64Encoder encoder = new BASE64Encoder();
+            result = encoder.encode(byteArrayCode);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * base64译码
+     * @param code
+     * @return
+     */
+    public static String base64Decode(String code) {
+        String result = null;
+        try {
+            BASE64Decoder decoder = new BASE64Decoder();
+            result = new String(decoder.decodeBuffer(code), "utf-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 字符串转ASCII
+     * @param value
+     * @return
+     */
+    public static String stringToAscii(String value)
+    {
+        StringBuffer sbu = new StringBuffer();
+        char[] chars = value.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if(i != chars.length - 1)
+            {
+                sbu.append((int)chars[i]).append(",");
+            }
+            else {
+                sbu.append((int)chars[i]);
+            }
+        }
+        return sbu.toString();
+    }
+
+    /**
+     * ASCII加密并转为字符串
+     * @param ascii
+     * @param num
+     * @return
+     */
+    public static String encodeASCII(String ascii, Integer num) {
+        StringBuffer sbu = new StringBuffer();
+        String[] chars = ascii.split(",");
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = String.valueOf(Integer.parseInt(chars[i]) + num);
+            sbu.append((char) Integer.parseInt(chars[i]));
+        }
+        return sbu.toString();
+    }
+
+    /**
+     * ASCII解密并转为字符串
+     * @param ascii
+     * @param num
+     * @return
+     */
+    public static String decodeASCII(String ascii, Integer num) {
+        StringBuffer sbu = new StringBuffer();
+        String[] chars = ascii.split(",");
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = String.valueOf(Integer.parseInt(chars[i]) - num);
+            sbu.append((char) Integer.parseInt(chars[i]));
+        }
+        return sbu.toString();
+    }
+
+    /**
+     * 自写加密
+     * @param code
+     * @param num
+     * @return
+     */
+    public static String myEncode(String code, Integer num) {
+        String result = "";
+        try {
+            String b64En = base64Encode(code);
+            String ascii = stringToAscii(b64En);
+            result = encodeASCII(ascii, num);
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 自写解密
+     * @param code
+     * @param num
+     * @return
+     */
+    public static String myDecode(String code, Integer num) {
+        String result = "";
+        try {
+            String ascii = stringToAscii(code);
+            String b64De = decodeASCII(ascii, num);
+            result = base64Decode(b64De);
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
